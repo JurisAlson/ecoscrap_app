b5f64c37078c27aa24aefc803ecb066e6998e181
JurisAlson <jurisalson@gmail.com>
2026-03-22 18:43:48 +0800

    gg

--- lib/auth/login_page.dart
+++ lib/auth/login_page.dart
@@ -2,7 +2,6 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'dart:async';
 import 'forgot_password.dart';
